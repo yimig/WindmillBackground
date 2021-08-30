276b1cac60c2021f1b6cae299f0b87c34f7f3d41
yimig <zhangyiming@programmer.net>
2021-08-30 21:48:37 +0800

    init

---
+++ src/components/Comp/Comp.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="flex h-full w-full justify-center items-center bg-yellow-light">
+    <div class="justify-center items-center text-5xl relative">
+      <WindmillMatrix></WindmillMatrix>
+    </div>
+  </div>
+</template>
+
+<script>
+import WindmillMatrix from "../WindmillMatrix/WindmillMatrix.vue";
+export default {
+  name: "Comp",
+  props:['text'],
+  data(){
+    return{
+    }
+  },
+  components:{
+    WindmillMatrix,
+  },
+  computed:{
+  }
+}
+</script>
+<style lang="less" scoped>
+@import "res/style/comp.less";
+.text_shadow{
+  opacity: 0.2;
+}
+</style>
